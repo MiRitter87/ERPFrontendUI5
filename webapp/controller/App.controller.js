90f7547af8ba5cc3f4d312746c0471bd5471b432
Michael <michaelritter@gmx.net>
2020-12-02 11:08:59 +0100

    Navigate to scroll containers on menu item click.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,11 +5,22 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.App", {
+		/**
+		 * Initializes the controller.
+		 */
 		onInit : function() {
 			this.oModel = new JSONModel();
 			this.oModel.loadData("model/model.json");
 			this.getView().setModel(this.oModel);
-		}
+		},
+		
+		/**
+		 * Handles the selection of an item in the navigation menu.
+		 */
+		onItemSelect : function(oEvent) {
+			var item = oEvent.getParameter('item');
+			this.byId("pageContainer").to(this.getView().createId(item.getKey()));
+		},
 	});
 
 });
