2879bba483ce0362a04b20b4a8033568993f4762
Michael <michaelritter@gmx.net>
2020-12-03 10:06:45 +0100

    Application configuration moved to manifest file.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,18 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
   	"use strict";
 	return UIComponent.extend("ERPFrontendUI5.Component", {
 
 		metadata : {
-         	rootView: {
-            "viewName": "ERPFrontendUI5.view.App",
-            "type": "XML",
-            "async": true,
-            "id": "app"
-         	}
+         	manifest: "json"
       	},	
 
 		/**
@@ -26,14 +20,6 @@ sap.ui.define([
 			var oModel = new JSONModel();
 			oModel.loadData("model/model.json");
 			this.setModel(oModel);
-
-			//set i18n model
-         	var i18nModel = new ResourceModel({
-            	bundleName: "ERPFrontendUI5.i18n.i18n",
-            	supportedLocales: ["de", "en"],
-            	fallbackLocale: "de"
-         	});
-         	this.setModel(i18nModel, "i18n");
       	}
    	});
 });
