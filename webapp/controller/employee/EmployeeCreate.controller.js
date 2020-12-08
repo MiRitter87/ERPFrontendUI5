126ba0b73b00b8ef0036b2cc95096e0dde2eee39
Michael <michaelritter@gmx.net>
2020-12-08 11:38:39 +0100

    Component layout of employee creation view without functionality yet.

--- webapp/controller/employee/EmployeeCreate.controller.js
+++ webapp/controller/employee/EmployeeCreate.controller.js
@@ -1,11 +1,33 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller", 
+	"sap/ui/core/Item"
+], function (Controller, Item) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.employee.EmployeeCreate", {
+		/**
+		 * Initializes the controller.
+		 */
 		onInit : function () {
-
+			this.initializeGenderComboBox();
+		},
+		
+		
+		/**
+		 * Initializes the ComboBox for gender selection with the description texts in the correct language.
+		 */
+		initializeGenderComboBox : function () {
+			var comboBoxItem = new Item();
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			comboBoxItem.setKey("MALE");
+			comboBoxItem.setText(oResourceBundle.getText("gender.male"));
+			this.getView().byId("genderComboBox").addItem(comboBoxItem);
+			
+			comboBoxItem = new Item();
+			comboBoxItem.setKey("FEMALE");
+			comboBoxItem.setText(oResourceBundle.getText("gender.female"));
+			this.getView().byId("genderComboBox").addItem(comboBoxItem);
 		}
 	});
 
