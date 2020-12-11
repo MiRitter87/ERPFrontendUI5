b325993e6193bb49224cfb23255d0c371e6c1f8e
Michael <michaelritter@gmx.net>
2020-12-11 11:25:33 +0100

    Layout employee edit form and fill input fields on employee selection.

--- webapp/controller/employee/EmployeeEdit.controller.js
+++ webapp/controller/employee/EmployeeEdit.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-], function (Controller) {
+	"sap/ui/core/Item",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, Item, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.employee.EmployeeEdit", {
@@ -8,7 +11,76 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
+			this.queryEmployeeWebService();
+			this.initializeGenderComboBox();
+		},
+		
+		
+		/**
+		 * Initializes the ComboBox for gender selection with the description texts in the correct language.
+		 */
+		initializeGenderComboBox : function () {
+			var comboBoxItem = new Item();
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			comboBoxItem.setKey("MALE");
+			comboBoxItem.setText(oResourceBundle.getText("gender.male"));
+			this.getView().byId("genderComboBox").addItem(comboBoxItem);
+			
+			comboBoxItem = new Item();
+			comboBoxItem.setKey("FEMALE");
+			comboBoxItem.setText(oResourceBundle.getText("gender.female"));
+			this.getView().byId("genderComboBox").addItem(comboBoxItem);
+		},
+		
+		
+		/**
+		 * Handles the selection of an item in the employee ComboBox.
+		 */
+		onEmployeeSelectionChange : function (oControlEvent) {
+			var selectedItem = oControlEvent.getParameters().selectedItem;
+			var oModel = this.getView().getModel();
+			var employees = oModel.oData.employees;
+			var employee;
+			
+			//Get the selected employee from the array of all employees according to the id.
+			for(var i = 0; i < employees.data.employee.length; i++) {
+    			var tempEmployee = employees.data.employee[i];
+    			
+				if(tempEmployee.id == selectedItem.getKey()) {
+					employee = tempEmployee;
+				}
+			}
+			
+			//Set the model of the view according to the selected employee to allow binding of the UI elements.
+			oModel.setData({employees:employees, selectedEmployee : employee});
+		},
+		
+		
+		/**
+		 * Queries the employee WebService. If the call is successful, the model is updated with the employee data.
+		 */
+		queryEmployeeWebService : function() {
+			var webServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/employee");
+			var queryUrl = webServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : queryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({employees : data}); // not aData
+					
+					if(data.data != null) {
+						MessageToast.show(oResourceBundle.getText("employeeEdit.dataLoaded"));
+					}
+					else {
+						if(data.message != null)
+							MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		}
 	});
 
