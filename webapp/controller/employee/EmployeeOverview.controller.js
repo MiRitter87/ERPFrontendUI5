5280986533075633085c9fd8454b08608f0a709c
Michael <michaelritter@gmx.net>
2020-12-11 09:41:46 +0100

    Fill employee overview table with employees provided by WebService.

--- webapp/controller/employee/EmployeeOverview.controller.js
+++ webapp/controller/employee/EmployeeOverview.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.employee.EmployeeOverview", {
@@ -8,8 +10,35 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
+			this.queryEmployeeWebService();
+		},
+		
+		
+		/**
+		 * Queries the employee WebService. If the call is successful, the model is updated with the employee data.
+		 */
+		queryEmployeeWebService : function() {
+			var webServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/employee");
+			var queryUrl = webServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : queryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({employees : data}); // not aData
+					
+					if(data.data != null) {
+						MessageToast.show(oResourceBundle.getText("employeeOverview.dataLoaded"));
+					}
+					else {
+						if(data.message != null)
+							MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
 			
-		}
+			this.getView().setModel(oModel);
+		},
 	});
 
 });
