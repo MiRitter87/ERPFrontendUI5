94218c5d5220dd874b63ec134cea8666864ebc74
Michael <michaelritter@gmx.net>
2021-01-26 10:43:30 +0100

    Initialize ComboBox for head of department selection.

--- webapp/controller/department/DepartmentCreate.controller.js
+++ webapp/controller/department/DepartmentCreate.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.department.DepartmentCreate", {
@@ -8,7 +10,8 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
-			
+			//Load all employees. Those are the candidates that can be selected in the head selection ComboBox.
+			this.queryEmployeeWebService();
 		},
 		
 		
@@ -27,6 +30,29 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			
 			oRouter.navTo("startPageRoute");	
+		},
+		
+		
+		/**
+		 * Queries the employee WebService. If the call is successful, the model is updated with the employee data.
+		 */
+		queryEmployeeWebService : function() {
+			var webServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/employee");
+			var queryUrl = webServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : queryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({employees : data}); // not aData
+					
+					if(data.data == null && data.message != null)  {
+						MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		},
 	});
 });
