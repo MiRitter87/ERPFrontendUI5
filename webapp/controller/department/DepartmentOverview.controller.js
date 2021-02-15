8788a197844f4ddaf5c40164e8e11075b984ad65
Michael <michaelritter@gmx.net>
2021-02-15 09:25:51 +0100

    Query departments for overview via WebService and display result.

--- webapp/controller/department/DepartmentOverview.controller.js
+++ webapp/controller/department/DepartmentOverview.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.department.DepartmentOverview", {
@@ -8,8 +10,19 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
-			
+			//Register an event handler that gets called every time the router navigates to this view.
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("departmentOverviewRoute").attachMatched(this._onRouteMatched, this);
 		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function (oEvent) {
+			//Query department data every time a user navigates to this view. This assures that changes are being displayed in the table.
+			this.queryDepartmentWebService();
+    	},
 		
 		
 		/**
@@ -17,6 +30,33 @@ sap.ui.define([
 		 */
 		onDeletePressed : function () {
 			
+		},
+		
+		
+		/**
+		 * Queries the department WebService. If the call is successful, the model is updated with the department data.
+		 */
+		queryDepartmentWebService : function() {
+			var sWebServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/department");
+			var sQueryUrl = sWebServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : sQueryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({departments : data}); // not aData
+					
+					if(data.data != null) {
+						MessageToast.show(oResourceBundle.getText("departmentOverview.dataLoaded"));							
+					}
+					else {
+						if(data.message != null)
+							MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		},
 	});
 
