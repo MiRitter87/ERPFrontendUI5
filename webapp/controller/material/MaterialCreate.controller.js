c541bd16d37234eff72d24d065207e369a866dc7
Michael <michaelritter@gmx.net>
2021-02-23 11:41:23 +0100

    Basic view elements for material creation added.

--- webapp/controller/material/MaterialCreate.controller.js
+++ webapp/controller/material/MaterialCreate.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.material.MaterialCreate", {
@@ -8,7 +9,26 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oView, oMessageManager;
 			
-		}
+			//Initialize message manager for input form validation.
+			oView = this.getView();
+			oMessageManager = sap.ui.getCore().getMessageManager();
+			oView.setModel(oMessageManager.getMessageModel(), "message");
+			oMessageManager.registerObject(oView, true);
+			
+			this.initializeMaterialModel();
+		},
+		
+		
+		/**
+		 * Initializes the model to which the UI controls are bound.
+		 */
+		initializeMaterialModel : function () {
+			var oMaterialModel = new JSONModel();
+			
+			oMaterialModel.loadData("model/material/materialCreate.json");
+			this.getView().setModel(oMaterialModel);
+		},
 	});
 });
