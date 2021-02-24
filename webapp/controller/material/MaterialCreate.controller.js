d304619872fff41ef74b72f58e3d61e60830f1bd
Michael <michaelritter@gmx.net>
2021-02-24 10:46:57 +0100

    Validate material price in creation view.

--- webapp/controller/material/MaterialCreate.controller.js
+++ webapp/controller/material/MaterialCreate.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/NumberFormat"
+], function (Controller, JSONModel, NumberFormat) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.material.MaterialCreate", {
@@ -28,7 +29,49 @@ sap.ui.define([
 			var oMaterialModel = new JSONModel();
 			
 			oMaterialModel.loadData("model/material/materialCreate.json");
-			this.getView().setModel(oMaterialModel);
+			this.getView().setModel(oMaterialModel, "newMaterial");
 		},
+		
+		
+		/**
+		 * Handles a click at the save button.
+		 */
+		onSavePressed : function () {
+			this.validatePriceInput();
+		},
+		
+		
+		/**
+		 * Handles a click at the cancel button.
+		 */
+		onCancelPressed : function () {
+			
+		},
+		
+		
+		/**
+		 * Validates the price input field.
+		 *
+		 * There is a bug in german locale when defining an Input as Number of type float.
+		 * This is because the framework has a problem with the german delimiter ',' for fractional digits.
+		 * See ticket here: https://github.com/SAP/openui5/issues/2558.
+		 *
+         * Therefore the Input is set as type String and the price is parsed manually in this function.
+		 */
+		validatePriceInput : function () {
+			var sPriceInputString = this.getView().byId("priceInput").getValue();
+			var fPricePerUnit = parseFloat(sPriceInputString);
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			if(isNaN(fPricePerUnit)) {
+				this.getView().byId("priceInput").setValueState(sap.ui.core.ValueState.Error);
+				this.getView().byId("priceInput").setValueStateText(oResourceBundle.getText("departmentCreate.useDecimalPlacesError"));
+				this.getView().getModel("newMaterial").setProperty("/pricePerUnit", 0);
+			}
+			else {
+				this.getView().byId("priceInput").setValueState(sap.ui.core.ValueState.None);
+				this.getView().getModel("newMaterial").setProperty("/pricePerUnit", fPricePerUnit);			
+			}
+		}
 	});
 });
