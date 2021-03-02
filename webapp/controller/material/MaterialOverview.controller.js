c57f8ff1ae53d72c15530a1a799792a44f1af8c8
Michael <michaelritter@gmx.net>
2021-03-02 10:46:08 +0100

    Delete function in material overview implemented.

--- webapp/controller/material/MaterialOverview.controller.js
+++ webapp/controller/material/MaterialOverview.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
+	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.material.MaterialOverview", {
@@ -29,7 +30,38 @@ sap.ui.define([
 		 * Handles the press-event of the delete button.
 		 */
 		onDeletePressed : function () {
+			var oResourceBundle;
+			oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(this.isMaterialSelected() == false) {
+				MessageBox.error(oResourceBundle.getText("materialOverview.noMaterialSelected"));
+				return;
+			}
+			
+			this.deleteMaterial(this.getSelectedMaterial());
+		},
+		
+		
+		/**
+		 * Checks if a material has been selected.
+		 */
+		isMaterialSelected : function () {
+			if(this.getView().byId("materialTable").getSelectedItem() == null)
+				return false;
+			else
+				return true;
+		},
+		
+		
+		/**
+		 * Gets the the selected material.
+		 */
+		getSelectedMaterial : function () {
+			var oListItem = this.getView().byId("materialTable").getSelectedItem();
+			var oContext = oListItem.getBindingContext();
+			var oSelectedMaterial = oContext.getProperty(null, oContext);
+			
+			return oSelectedMaterial;
 		},
 		
 		
@@ -59,6 +91,36 @@ sap.ui.define([
 			});                                                                 
 			
 			this.getView().setModel(oModel);
+		},
+		
+		
+		/**
+		 * Deletes the given material using the WebService.
+		 */
+		deleteMaterial : function(oMaterial) {
+			var sWebServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/material");
+			var sQueryUrl = sWebServiceBaseUrl + "/" + oMaterial.id;
+			
+			//Use "DELETE" to delete an existing resource.
+			var aData = jQuery.ajax({type : "DELETE", contentType : "application/json", url : sQueryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					if(data.message != null) {
+						if(data.message[0].type == 'S') {
+							MessageToast.show(data.message[0].text);
+							this.queryMaterialWebService(false);
+						}
+						
+						if(data.message[0].type == 'E') {
+							MessageBox.error(data.message[0].text);
+						}
+						
+						if(data.message[0].type == 'W') {
+							MessageBox.warning(data.message[0].text);
+						}
+					}
+				},
+				context : this
+			}); 
 		}
 	});
 
