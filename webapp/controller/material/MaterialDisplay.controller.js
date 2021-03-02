cbe48ac2aaff86575af41d9c36fa76926fa31590
Michael <michaelritter@gmx.net>
2021-03-02 09:36:24 +0100

    Load materials in display view and show data on selection.

--- webapp/controller/material/MaterialDisplay.controller.js
+++ webapp/controller/material/MaterialDisplay.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.material.MaterialDisplay", {
@@ -8,15 +10,72 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
-			
+			//Register an event handler that gets called every time the router navigates to this view.
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("materialDisplayRoute").attachMatched(this._onRouteMatched, this);
 		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function (oEvent) {
+			//Query material data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			this.queryMaterialWebService();
+			this.getView().byId("materialComboBox").setSelectedItem(null);
+    	},
 		
 		
 		/**
 		 * Handles the selection of an item in the material ComboBox.
 		 */
 		onMaterialSelectionChange : function (oControlEvent) {
+			var oSelectedItem = oControlEvent.getParameters().selectedItem;
+			var oModel = this.getView().getModel();
+			var oMaterials = oModel.oData.materials;
+			var oMaterial;
 			
+			if(oSelectedItem == null)
+				return;
+			
+			//Get the selected material from the array of all materials according to the id.
+			for(var i = 0; i < oMaterials.data.material.length; i++) {
+    			var oTempMaterial = oMaterials.data.material[i];
+    			
+				if(oTempMaterial.id == oSelectedItem.getKey()) {
+					oMaterial = oTempMaterial;
+				}
+			}
+			
+			//Set the model of the view according to the selected material to allow binding of the UI elements.
+			oModel.setData({selectedMaterial : oMaterial}, true);
+		},
+		
+		
+		/**
+		 * Queries the material WebService. If the call is successful, the model is updated with the department data.
+		 */
+		queryMaterialWebService : function() {
+			var sWebServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/material");
+			var sQueryUrl = sWebServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : sQueryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({materials : data}); // not aData
+					
+					if(data.data != null) {
+						MessageToast.show(oResourceBundle.getText("materialDisplay.dataLoaded"));
+					}
+					else {
+						if(data.message != null)
+							MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		}
 	});
 });
