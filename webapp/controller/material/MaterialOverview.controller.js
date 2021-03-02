2c64b567e794798c7dcdb0135324975026fbf144
Michael <michaelritter@gmx.net>
2021-03-02 10:35:19 +0100

    Load materials for overview using WebService.

--- webapp/controller/material/MaterialOverview.controller.js
+++ webapp/controller/material/MaterialOverview.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.material.MaterialOverview", {
@@ -8,8 +10,19 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
-			
+			//Register an event handler that gets called every time the router navigates to this view.
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("materialOverviewRoute").attachMatched(this._onRouteMatched, this);
 		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function (oEvent) {
+			//Query material data every time a user navigates to this view. This assures that changes are being displayed in the table.
+			this.queryMaterialWebService(true);
+    	},
 		
 		
 		/**
@@ -17,6 +30,35 @@ sap.ui.define([
 		 */
 		onDeletePressed : function () {
 			
+		},
+		
+		
+		/**
+		 * Queries the material WebService. If the call is successful, the model is updated with the material data.
+		 */
+		queryMaterialWebService : function(bShowSuccessMessage) {
+			var sWebServiceBaseUrl = this.getOwnerComponent().getModel("webServiceBaseUrls").getProperty("/material");
+			var sQueryUrl = sWebServiceBaseUrl + "/";
+			var oModel = new JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", url : sQueryUrl, dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+					oModel.setData({materials : data}); // not aData
+					
+					if(data.data != null) {
+						if(bShowSuccessMessage == true) {
+							MessageToast.show(oResourceBundle.getText("materialOverview.dataLoaded"));
+						}					
+					}
+					else {
+						if(data.message != null)
+							MessageToast.show(data.message[0].text);
+					}
+				},
+				context : this
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		}
 	});
 
