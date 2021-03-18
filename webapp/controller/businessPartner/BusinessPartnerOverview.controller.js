0f5ff6e92bb0a8873c4feebb48ecf57e94807b62
Michael <michaelritter@gmx.net>
2021-03-18 09:18:38 +0100

    Load and display business partners in overview.

--- webapp/controller/businessPartner/BusinessPartnerOverview.controller.js
+++ webapp/controller/businessPartner/BusinessPartnerOverview.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"./BusinessPartnerController"
+], function (Controller, MessageToast, JSONModel, BusinessPartnerController) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.businessPartner.BusinessPartnerOverview", {
@@ -8,8 +11,19 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
-
+			//Register an event handler that gets called every time the router navigates to this view.
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("businessPartnerOverviewRoute").attachMatched(this._onRouteMatched, this);
 		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query business partner data every time a user navigates to this view. This assures that changes are being displayed in the table.
+			BusinessPartnerController.queryBusinessPartnersByWebService(this.queryBusinessPartnersCallback, this, true);
+    	},
 		
 		
 		/**
@@ -17,6 +31,29 @@ sap.ui.define([
 		 */
 		onDeletePressed : function () {
 			
-		}
+		},
+		
+		
+		/**
+		 * Callback function of the queryBusinessPartners RESTful WebService call in the BusinessPartnerController.
+		 */
+		queryBusinessPartnersCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			oModel.setData({businessPartners : oReturnData});
+			
+			if(oReturnData.data != null) {
+				if(bShowSuccessMessage == true) {
+					MessageToast.show(oResourceBundle.getText("businessPartnerOverview.dataLoaded"));
+				}					
+			}
+			else {
+				if(oReturnData.message != null)
+					MessageToast.show(oReturnData.message[0].text);
+			}
+
+			oCallingController.getView().setModel(oModel);
+		},
 	});
 });
