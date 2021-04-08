5537a55e9a7f24cae5fe6c9614035882c6079cb7
Michael <michaelritter@gmx.net>
2021-04-08 11:56:23 +0200

    Open and close dialog for sales order item creation.

--- webapp/controller/salesOrder/SalesOrderCreate.controller.js
+++ webapp/controller/salesOrder/SalesOrderCreate.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/Fragment"
+], function (Controller, Fragment) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.salesOrder.SalesOrderCreate", {
@@ -9,6 +10,47 @@ sap.ui.define([
 		 */
 		onInit : function () {
 						
+		},
+		
+		
+		/**
+		 * Opens the dialog to add a new sales order item.
+		 */
+		onAddItemPressed : function () {
+			var oView = this.getView();
+
+			//create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = Fragment.load({
+					id: oView.getId(),
+					name: "ERPFrontendUI5.view.salesOrder.SalesOrderItemCreate",
+					controller: this
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		},
+
+
+		/**
+		 * Handles the closing of the new item Dialog.
+		 */
+		onSaveDialog : function () {
+			this.byId("newItemDialog").close();
+		},
+
+		
+		/**
+		 * Handles the closing of the new item Dialog.
+		 */
+		onCancelDialog : function () {
+			this.byId("newItemDialog").close();
 		}
 	});
 });
