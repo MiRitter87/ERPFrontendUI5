b47dd775362383b6addcf4e8afea7eabcf97211a
Michael <michaelritter@gmx.net>
2021-04-26 09:49:22 +0200

    Bind partner ComboBoxes to model of selected sales order.

--- webapp/controller/salesOrder/SalesOrderEdit.controller.js
+++ webapp/controller/salesOrder/SalesOrderEdit.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"./SalesOrderController",
+	"../businessPartner/BusinessPartnerController",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
-], function (Controller, SalesOrderController, JSONModel, MessageToast) {
+], function (Controller, SalesOrderController, BusinessPartnerController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.salesOrder.SalesOrderEdit", {		
@@ -20,7 +21,8 @@ sap.ui.define([
 		 * Handles the routeMatched-event when the router navigates to this view.
 		 */
 		_onRouteMatched: function () {
-			//Query sales order data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			//Query sales order and business partner data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			BusinessPartnerController.queryBusinessPartnersByWebService(this.queryBusinessPartnersCallback, this);
 			SalesOrderController.querySalesOrdersByWebService(this.querySalesOrdersCallback, this, true);
     	},
 
@@ -73,6 +75,24 @@ sap.ui.define([
 			}                                                               
 			
 			oCallingController.getView().setModel(oModel, "salesOrders");
+		},
+		
+		
+		/**
+		 * Callback function of the queryBusinessPartners RESTful WebService call in the BusinessPartnerController.
+		 */
+		queryBusinessPartnersCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);				
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "businessPartners");
 		},
 		
 		
