97ad8239467495cc86a62ed3f51b5b008e935cb1
Michael <michaelritter@gmx.net>
2021-05-05 09:32:57 +0200

    Load data for sales order overview by WebService and bind to UI.

--- webapp/controller/salesOrder/SalesOrderOverview.controller.js
+++ webapp/controller/salesOrder/SalesOrderOverview.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../../model/formatter"
-], function (Controller, formatter) {
+	"./SalesOrderController",
+	"../../model/formatter",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, SalesOrderController, formatter, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.salesOrder.SalesOrderOverview", {
@@ -11,7 +14,39 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("salesOrderOverviewRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			SalesOrderController.querySalesOrdersByWebService(this.querySalesOrdersCallback, this, true);
+    	},
+
+
+		/**
+		 * Callback function of the querySalesOrder RESTful WebService call in the SalesOrderController.
+		 */
+		querySalesOrdersCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("salesOrderOverview.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "salesOrders");
 		}
 	});
 });
