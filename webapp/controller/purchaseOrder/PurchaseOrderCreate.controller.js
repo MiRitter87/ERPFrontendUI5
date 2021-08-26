ae29ddac911c73dda8a4014f2c3624bea3af3a6c
Michael <michaelritter@gmx.net>
2021-08-26 08:42:18 +0200

    Initialize model for purchase order creation.

--- webapp/controller/purchaseOrder/PurchaseOrderCreate.controller.js
+++ webapp/controller/purchaseOrder/PurchaseOrderCreate.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"../businessPartner/BusinessPartnerController",
+	"./PurchaseOrderController",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, BusinessPartnerController, JSONModel) {
+], function (Controller, BusinessPartnerController, PurchaseOrderController, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.purchaseOrder.PurchaseOrderCreate", {
@@ -24,7 +25,28 @@ sap.ui.define([
 		_onRouteMatched: function () {
 			//Query business partner and material data every time a user navigates to this view. This assures that changes are being displayed in the ComboBoxes.
 			BusinessPartnerController.queryBusinessPartnersByWebService(this.queryBusinessPartnersCallback, this, false, "VENDOR");
+			
+			this.initializePurchaseOrderModel();
+			PurchaseOrderController.initializePurchaseOrderItemModel(this);
     	},
+
+
+		/**
+		 * Initializes the model of the purchase order to which the UI controls are bound.
+		 */
+		initializePurchaseOrderModel : function () {
+			var oPurchaseOrderModel = new JSONModel();
+			
+			//Load and set order model
+			oPurchaseOrderModel.loadData("model/purchaseOrder/purchaseOrderCreate.json");
+			oPurchaseOrderModel.attachRequestCompleted(function() {
+				//Wait for date initialization until the JSON data have been loaded. Otherwise the date would be overwritten.
+				oPurchaseOrderModel.setProperty("/orderDate", new Date());
+				oPurchaseOrderModel.setProperty("/requestedDeliveryDate", new Date());
+   			 });
+			
+			this.getView().setModel(oPurchaseOrderModel, "newPurchaseOrder");	
+		},
 
 
 		/**
