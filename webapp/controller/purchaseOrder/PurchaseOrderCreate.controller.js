9bca89b082663eb51366dc0afbf28f5e7907fe93
Michael <michaelritter@gmx.net>
2021-08-26 08:21:23 +0200

    Load vendors on navigation to purchase order creation view.

--- webapp/controller/purchaseOrder/PurchaseOrderCreate.controller.js
+++ webapp/controller/purchaseOrder/PurchaseOrderCreate.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"../businessPartner/BusinessPartnerController",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, BusinessPartnerController, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.purchaseOrder.PurchaseOrderCreate", {
@@ -8,7 +10,38 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			//Register an event handler that gets called every time the router navigates to this view.
+			var oRouter;
 			
+			oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("purchaseOrderCreateRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query business partner and material data every time a user navigates to this view. This assures that changes are being displayed in the ComboBoxes.
+			BusinessPartnerController.queryBusinessPartnersByWebService(this.queryBusinessPartnersCallback, this, false, "VENDOR");
+    	},
+
+
+		/**
+		 * Callback function of the queryBusinessPartners RESTful WebService call in the BusinessPartnerController.
+		 */
+		queryBusinessPartnersCallback : function(oReturnData, oCallingController) {
+			var oModel = new JSONModel();
+			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);				
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "businessPartners");
 		}
 	});
 });
