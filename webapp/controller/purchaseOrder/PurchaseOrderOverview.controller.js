4823486f0cc52ce370bd20a31123155f7864d7bf
Michael <michaelritter@gmx.net>
2021-09-10 09:37:12 +0200

    Load purchase orders on navigation to overview.

--- webapp/controller/purchaseOrder/PurchaseOrderOverview.controller.js
+++ webapp/controller/purchaseOrder/PurchaseOrderOverview.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./PurchaseOrderController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, PurchaseOrderController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.purchaseOrder.PurchaseOrderOverview", {
@@ -8,7 +11,47 @@ sap.ui.define([
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("purchaseOrderOverviewRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			PurchaseOrderController.queryPurchaseOrdersByWebService(this.queryPurchaseOrdersCallback, this, true);
+    	},
+
+
+		/**
+		 * Formatter of the purchase order total status state.
+		 */
+		totalStatusStateFormatter: function(aStatus) {
+			return PurchaseOrderController.totalStatusStateFormatter(aStatus);
+		},
+
+
+		/**
+		 * Callback function of the queryPurchaseOrder RESTful WebService call in the PurchaseOrderController.
+		 */
+		queryPurchaseOrdersCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("purchaseOrderOverview.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "purchaseOrders");
 		}
 	});
 });
