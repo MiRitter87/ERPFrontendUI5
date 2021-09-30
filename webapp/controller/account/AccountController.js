d27a61bdce5f4726d3728724a071a87d2b8f7d08
Michael <michaelritter@gmx.net>
2021-09-30 09:17:29 +0200

    Validate user input in account creation view.

--- webapp/controller/account/AccountController.js
+++ webapp/controller/account/AccountController.js
@@ -20,6 +20,46 @@ sap.ui.define([
 			oComboBoxItem.setKey(sItemKey);
 			oComboBoxItem.setText(oResourceBundle.getText(sTextKey));
 			oComboBox.addItem(oComboBoxItem);
+		},
+		
+		
+		/**
+		 * Validates the given balance input field.
+		 *
+		 * There is a bug in german locale when defining an Input as Number of type float.
+		 * This is because the framework has a problem with the german delimiter ',' for fractional digits.
+		 * See ticket here: https://github.com/SAP/openui5/issues/2558.
+		 *
+         * Therefore the Input is set as type String and the balance is parsed manually in this function.
+		 */
+		validateBalanceInput : function (oBalanceInput, oResourceBundle, oModel, sBalancePropertyPath) {
+			var sBalanceInputString = oBalanceInput.getValue();
+			var fBalance = parseFloat(sBalanceInputString);
+			
+			if(isNaN(fBalance)) {
+				oBalanceInput.setValueState(sap.ui.core.ValueState.Error);
+				oBalanceInput.setValueStateText(oResourceBundle.getText("account.useDecimalPlacesError"));
+				oModel.setProperty(sBalancePropertyPath, 0);
+			}
+			else {
+				oBalanceInput.setValueState(sap.ui.core.ValueState.None);
+				oModel.setProperty(sBalancePropertyPath, fBalance);			
+			}
+		},
+		
+		
+		/**
+		 * Checks if a valid balance is filled in.
+		 */
+		isBalanceValid : function (sBalanceInputString) {
+			var fBalance = parseFloat(sBalanceInputString);
+			
+			if(isNaN(fBalance)) {
+				return false;
+			}
+			else {
+				return true;
+			}
 		}
 	};
 });
--- webapp/controller/account/AccountCreate.controller.js
+++ webapp/controller/account/AccountCreate.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"./AccountController",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, AccountController, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox"
+], function (Controller, AccountController, JSONModel, MessageBox) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.account.AccountCreate", {
@@ -28,6 +29,35 @@ sap.ui.define([
 			this.resetFormFields();
 			this.initializeAccountModel();
     	},
+
+
+		/**
+		 * Handles a click at the save button.
+		 */
+		onSavePressed : function () {
+			//Validate balance first to remove the error indication from the input field as soon as possible if the user fills in correct data.
+			AccountController.validateBalanceInput(this.getView().byId("balanceInput"), this.getOwnerComponent().getModel("i18n").getResourceBundle(),
+				this.getView().getModel("newAccount"), "/balance");
+				
+			if(this.getView().byId("currencyComboBox").getSelectedKey() == "") {
+				this.showMessageOnUndefinedCurrency();
+				return;
+			}
+			
+			if(AccountController.isBalanceValid(this.getView().byId("balanceInput").getValue()) == false)
+				return;				
+			
+			/*MaterialController.createMaterialbyWebService(this.getView().getModel("newMaterial"), this.saveMaterialCallback, this);*/
+		},
+		
+		
+		/**
+		 * Displays a message in case the currency has not been selected.
+		 */
+		showMessageOnUndefinedCurrency : function () {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			MessageBox.error(oResourceBundle.getText("accountCreate.noCurrencySelected"));
+		},
 
 
 		/**
