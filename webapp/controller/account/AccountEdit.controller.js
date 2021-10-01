677ddbf3b84bf8a9460c1d8b838cc14e49f61c11
Michael <michaelritter@gmx.net>
2021-10-01 08:54:56 +0200

    Initialize account edit view on navigation to view.

--- webapp/controller/account/AccountEdit.controller.js
+++ webapp/controller/account/AccountEdit.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./AccountController"
+], function (Controller, AccountController) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.account.AccountEdit", {
@@ -8,7 +9,37 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter;
 			
+			//Register an event handler that gets called every time the router navigates to this view.
+			oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("accountEditRoute").attachMatched(this._onRouteMatched, this);
+			
+			AccountController.initializeCurrencyComboBox(this.getView().byId("currencyComboBox"), 
+				this.getOwnerComponent().getModel("i18n").getResourceBundle());
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			this.resetFormFields();
+    	},
+
+
+		/**
+		 * Resets the form fields to the initial state.
+		 */
+		resetFormFields : function () {
+			this.getView().byId("accountComboBox").setSelectedItem(null);
+			
+			this.getView().byId("idText").setText("");
+			this.getView().byId("descriptionTextArea").setValue("");
+			
+			this.getView().byId("balanceInput").setValue(0);
+			this.getView().byId("balanceInput").setValueState(sap.ui.core.ValueState.None);
+			this.getView().byId("currencyComboBox").setSelectedItem(null);
 		}
 	});
 });
