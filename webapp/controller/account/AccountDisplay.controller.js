a673b9dda5dbdcbe2433069ac535b7d7491cf042
Michael <michaelritter@gmx.net>
2021-10-05 08:38:14 +0200

    Initialize view and load data on navigation to account display view.

--- webapp/controller/account/AccountDisplay.controller.js
+++ webapp/controller/account/AccountDisplay.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./AccountController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, AccountController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.account.AccountDisplay", {
@@ -8,7 +11,58 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("accountDisplayRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			AccountController.queryAccountsByWebService(this.queryAccountsCallback, this, true);
 			
-		}
+			this.getView().setModel(null, "selectedAccount");
+			this.resetUIElements();
+    	},
+
+
+		/**
+		 * Callback function of the queryAccounts RESTful WebService call in the AccountController.
+		 */
+		queryAccountsCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("accountDisplay.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "accounts");
+		},
+		
+		
+		/**
+		 * Resets the UI elements.
+		 */
+		resetUIElements : function () {
+			this.getView().byId("accountComboBox").setSelectedItem(null);
+			
+			this.getView().byId("idText").setText("");
+			this.getView().byId("descriptionText").setText("");
+			
+			this.getView().byId("balanceText").setText("");
+			this.getView().byId("currencyText").setText("");
+			
+			this.getView().byId("postingTable").destroyItems();
+		},
 	});
 });
