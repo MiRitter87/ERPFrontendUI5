8d1c6ff5aa38e21c7e2b60a1971cace298f74b58
Michael <michaelritter@gmx.net>
2021-10-06 10:58:35 +0200

    Initialize account overview and load accounts via WebService.

--- webapp/controller/account/AccountOverview.controller.js
+++ webapp/controller/account/AccountOverview.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./AccountController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, AccountController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.account.AccountOverview", {
@@ -8,7 +11,39 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("accountOverviewRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			AccountController.queryAccountsByWebService(this.queryAccountsCallback, this, true);
+    	},
+
+
+		/**
+		 * Callback function of the queryAccounts RESTful WebService call in the AccountController.
+		 */
+		queryAccountsCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("accountOverview.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "accounts");
 		}
 	});
 });
