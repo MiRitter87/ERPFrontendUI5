752f7665bd11e5e5537983b614dd0587860e3656
Michael <michaelritter@gmx.net>
2021-11-24 09:16:54 +0100

    Define UI elements of bill of material creation view.

--- webapp/controller/billOfMaterial/BillOfMaterialCreate.controller.js
+++ webapp/controller/billOfMaterial/BillOfMaterialCreate.controller.js
@@ -9,6 +9,22 @@ sap.ui.define([
 		 */
 		onInit : function () {
 
-		}		
+		},
+		
+		
+		/**
+		 * Formatter of the material text in the item table. Provides the name of a material based on the given ID.
+		 */
+		materialNameFormatter : function(iMaterialId) {
+			return "TODO";
+		},
+		
+		
+		/**
+		 * Formatter of the material unit in the item table. Provides the unit of a material based on the given ID.
+		 */
+		materialUnitFormatter: function(iMaterialId) {
+			return "TODO";
+		},
 	});
 });
