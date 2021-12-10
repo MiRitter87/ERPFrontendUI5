50006fdc4136ff87122e11446179220a56e53a39
Michael <michaelritter@gmx.net>
2021-12-10 08:22:30 +0100

    Load and display data in bill of material overview.

--- webapp/controller/billOfMaterial/BillOfMaterialOverview.controller.js
+++ webapp/controller/billOfMaterial/BillOfMaterialOverview.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./BillOfMaterialController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, BillOfMaterialController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.billOfMaterial.BillOfMaterialOverview", {
@@ -8,7 +11,39 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("billOfMaterialOverviewRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			BillOfMaterialController.queryBillOfMaterialsByWebService(this.queryBillOfMaterialsCallback, this, true);
+    	},
+
+
+		/**
+		 * Callback function of the queryBillOfMaterials RESTful WebService call in the BillOfMaterialController.
+		 */
+		queryBillOfMaterialsCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
 			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("billOfMaterialOverview.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "billOfMaterials");
 		}
 	});
 });
