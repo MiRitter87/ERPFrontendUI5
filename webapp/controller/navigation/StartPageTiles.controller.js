19334f36791e483a139790a50e20033642ab53aa
Michael <michaelritter@gmx.net>
2022-01-07 09:01:35 +0100

    Implement tiles for navigation to employee views.

--- webapp/controller/navigation/StartPageTiles.controller.js
+++ webapp/controller/navigation/StartPageTiles.controller.js
@@ -6,7 +6,43 @@ sap.ui.define([
 	return Controller.extend("ERPFrontendUI5.controller.navigation.StartPageTiles", {
 		onInit : function () {
 
-		}
+		},
+		
+		
+		/**
+		 * Handles click at the employee create tile.
+		 */
+		onEmployeeCreatePressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("employeeCreateRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the employee edit tile.
+		 */
+		onEmployeeEditPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("employeeEditRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the employee display tile.
+		 */
+		onEmployeeDisplayPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("employeeDisplayRoute");	
+		},
+		
+		
+		/**
+		 * Handles click at the employee overview tile.
+		 */
+		onEmployeeOverviewPressed : function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("employeeOverviewRoute");	
+		},
 	});
 
 });
