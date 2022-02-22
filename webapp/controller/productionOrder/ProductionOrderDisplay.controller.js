b174627c11bcd9312eb4d2e97cbbe3e8fb8a670f
Michael <michaelritter@gmx.net>
2022-02-22 09:40:57 +0100

    Load production order data on navigation to display view.

--- webapp/controller/productionOrder/ProductionOrderDisplay.controller.js
+++ webapp/controller/productionOrder/ProductionOrderDisplay.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./ProductionOrderController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function (Controller, ProductionOrderController, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.productionOrder.ProductionOrderDisplay", {
@@ -8,7 +11,42 @@ sap.ui.define([
 		 * Initializes the controller.
 		 */
 		onInit : function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("productionOrderDisplayRoute").attachMatched(this._onRouteMatched, this);
+		},
+		
+		
+		/**
+		 * Handles the routeMatched-event when the router navigates to this view.
+		 */
+		_onRouteMatched: function () {
+			//Query master data every time a user navigates to this view. This assures that changes are being displayed in the ComboBox.
+			ProductionOrderController.queryProductionOrdersByWebService(this.queryProductionOrdersCallback, this, true);
 			
+			this.getView().setModel(null, "selectedProductionOrder");
+			//this.resetUIElements();
+    	},
+
+
+		/**
+		 * Callback function of the queryProductionOrders RESTful WebService call in the ProductionOrderController.
+		 */
+		queryProductionOrdersCallback : function(oReturnData, oCallingController, bShowSuccessMessage) {
+			var oModel = new JSONModel();
+			var oResourceBundle = oCallingController.getOwnerComponent().getModel("i18n").getResourceBundle();
+			
+			if(oReturnData.data != null) {
+				oModel.setData(oReturnData.data);
+				
+				if(bShowSuccessMessage == true)
+					MessageToast.show(oResourceBundle.getText("productionOrderDisplay.dataLoaded"));			
+			}
+			
+			if(oReturnData.data == null && oReturnData.message != null)  {
+				MessageToast.show(oReturnData.message[0].text);
+			}                                                               
+			
+			oCallingController.getView().setModel(oModel, "productionOrders");
 		}
 	});
 });
