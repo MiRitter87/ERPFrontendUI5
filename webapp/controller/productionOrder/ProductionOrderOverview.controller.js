251fabce026d76fe716d05536fd6e3625d521d93
Michael <michaelritter@gmx.net>
2022-02-23 08:53:36 +0100

    Add UI elements to production order overview.

--- webapp/controller/productionOrder/ProductionOrderOverview.controller.js
+++ webapp/controller/productionOrder/ProductionOrderOverview.controller.js
@@ -1,14 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"./ProductionOrderController",
+	"../../model/formatter"
+], function (Controller, ProductionOrderController, formatter) {
 	"use strict";
 
 	return Controller.extend("ERPFrontendUI5.controller.productionOrder.ProductionOrderOverview", {
+		formatter: formatter,
+		
+		
 		/**
 		 * Initializes the Controller.
 		 */
 		onInit : function () {
 			
-		}
+		},
+		
+		
+		/**
+		 * Formatter of the production order status text.
+		 */
+		orderStatusTextFormatter: function(sStatus) {
+			return ProductionOrderController.orderStatusTextFormatter(sStatus, this.getOwnerComponent().getModel("i18n").getResourceBundle());
+		},
+		
+		
+		/**
+		 * Formatter of the production order status state.
+		 */
+		orderStatusStateFormatter: function(sStatus) {
+			return ProductionOrderController.orderStatusStateFormatter(sStatus);
+		},
 	});
 });
